017961f32b97e6484282b12d3f06ab9bed7b87ad
iliami <ilia.mi1908@gmail.com>
2024-05-04 16:34:37 +0400

    Still configure DI

---
+++ PawsAndEars/NinjectModules/AppDbContextModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Modules;
+using PawsAndEars.EF;
+
+namespace PawsAndEars.NinjectModules
+{
+    public class AppDbContextModule : NinjectModule
+    {
+        private string connectionString;
+        public AppDbContextModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<AppDbContext>().To<AppDbContext>().WithConstructorArgument(connectionString);
+        }
+    }
+}
