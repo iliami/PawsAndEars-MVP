717ea3fcdbf64aac1457296aec4315b74670d663
iliami <ilia.mi1908@gmail.com>
2024-05-04 16:31:19 +0400

    Configure DI with Ninject

--- PawsAndEars/App_Start/RouteConfig.cs
+++ PawsAndEars/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace PawsAndEars
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Dogs",
+                url: "dogs/{action}",
+                defaults: new { controller = "Dogs", action = "GetAll" }
+            );
         }
     }
 }
