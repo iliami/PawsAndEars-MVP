f69d2d27618d5c7675eec2f86833460ea99bd786
iliami <ilia.mi1908@gmail.com>
2024-05-04 16:35:50 +0400

    Controllers and Views
    HomeController - main page
    DogsController - /dogs/GetAll to get list of dogs, /dogs/Add to add a dog

---
+++ PawsAndEars/Controllers/DogsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using PawsAndEars.EF;
+using PawsAndEars.EF.Interfaces;
+using PawsAndEars.Models;
+
+namespace PawsAndEars.Controllers
+{
+    public class DogsController : Controller
+    {
+        private readonly IRepository<Dog> repo;
+
+        public DogsController(IRepository<Dog> dogRepository)
+        {
+            repo = dogRepository;
+        }
+
+        // GET: Dogs
+        public async Task<ActionResult> GetAll()
+        {
+            var dogs = await repo.GetAll();
+            return View("All", dogs);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Add(Dog dog)
+        {
+            repo.Save(dog);
+            var dogs = await repo.GetAll();
+            return View("All", dogs);
+        }
+    }
+}
