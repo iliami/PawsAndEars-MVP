0b94a22f7d3ff23df3b2c04f3d5ea2dc2936b73e
iliami <ilia.mi1908@gmail.com>
2024-05-13 11:47:05 +0400

    Add breed to db models

--- PawsAndEars/EF/AppDbContext.cs
+++ PawsAndEars/EF/AppDbContext.cs
@@ -11,6 +11,7 @@ namespace PawsAndEars.EF
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Dog> Dogs { get; set; }
+        public DBSet<Breed> Breeds { get; set; }
         public DbSet<Disease> Diseases { get; set; }
         public DbSet<Food> Foods { get; set; }
         public DbSet<Training> Trainings { get; set; }
--- PawsAndEars/EF/DbInitializer.cs
+++ PawsAndEars/EF/DbInitializer.cs
@@ -13,12 +13,19 @@ namespace PawsAndEars.EF
         {
             Disease disease = new Disease() { Id = 1, Name = "Puppy-poe syndrom", Description = "Abashapotyq" };
 
+            List<Breed> breeds = new List<Breed>()
+            {
+                new Breed() { Id = 1, Name = "A", MealsPerDay = 2, WalkingMinutesPerDay=120},
+                new Breed() { Id = 2, Name = "AA", MealsPerDay = 3, WalkingMinutesPerDay=150},
+                new Breed() { Id = 3, Name = "AAA", MealsPerDay = 4, WalkingMinutesPerDay=180}
+            };
+
             User user = new User() { Id = 1, Name = "Joe", StartWorkingTime = new DateTime(2024, 1, 1, 8, 0, 0), EndWorkingTime = new DateTime(2024, 1, 1, 17, 0, 0) };
 
             List<Dog> dogs = new List<Dog>() 
             {
-                new Dog() { Id = 1, Name = "A", User = user, UserId = 1, Age = 12, Breed = "A", Length = 100, Weight = 10 }
-            ,   new Dog() { Id = 2, Name = "AA", User = user, UserId = 1, Age = 24, Breed = "AA", Length = 150, Weight = 15, Diseases = new List<Disease>() { disease } }
+                new Dog() { Id = 1, Name = "A", User = user, UserId = 1, Age = 12, Breed = breeds[0], Length = 100, Weight = 10 }
+            ,   new Dog() { Id = 2, Name = "AA", User = user, UserId = 1, Age = 24, Breed = breeds[1], Length = 150, Weight = 15, Diseases = new List<Disease>() { disease } }
             };
 
             Food food = new Food() { Id = 1, Name = "Cracker", Description = "Tasty crackers for your dog", Weight = 1000, CaloriesPer100g = 250, Price = 5.87m };
@@ -36,6 +43,7 @@ namespace PawsAndEars.EF
             };
 
             context.Diseases.Add(disease);
+            context.Breeds.Add(breeds);
             context.Foods.Add(food);
             context.Trainings.Add(training);
             context.Users.Add(user);
