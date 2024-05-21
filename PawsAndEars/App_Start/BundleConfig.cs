ab98eb2317882ea1f7f33f348c7e0f022aff1fdc
iliami <ilia.mi1908@gmail.com>
2024-05-21 22:20:58 +0400

    Add css

--- PawsAndEars/App_Start/BundleConfig.cs
+++ PawsAndEars/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace PawsAndEars
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/PawsAndEars.css"));
         }
     }
 }
